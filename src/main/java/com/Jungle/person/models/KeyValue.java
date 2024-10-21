ff3396ae7967819709bd83a20e02a04e759335e8
Jungle430 <junglece430@gmail.com>
2024-10-21 16:07:23 +0800

    mod sort

--- src/main/java/com/Jungle/person/models/KeyValue.java
+++ src/main/java/com/Jungle/person/models/KeyValue.java
@@ -4,6 +4,6 @@ package com.Jungle.person.models;
 public record KeyValue(String key, String value) implements Comparable<KeyValue> {
     @Override
     public int compareTo(KeyValue o) {
-        return 0;
+        return key.compareTo(o.key);
     }
 }
